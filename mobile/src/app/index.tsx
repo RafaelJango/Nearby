6d20a57faf9292fce3bb5b77bfed4edcf7a65f3a
Jango <rafaeljangodev@gmail.com>
2024-12-11 15:23:11 -0300

    Add: Categories and Places

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,8 +1,9 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { router } from "expo-router";
 
 import { Welcome } from "@/components/welcome";
 import { Steps } from "@/components/steps";
-import { Step } from "@/components/step";
+import { Button } from "@/components/button";
 
 export default function Index() {
   return (
@@ -15,6 +16,10 @@ export default function Index() {
     >
       <Welcome />
       <Steps />
+
+      <Button onPress={() => router.navigate("/home")}>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   );
 }
